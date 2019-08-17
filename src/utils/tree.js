5285b0aa04ea0314700b5cebb60078f10cc98ca5
Jeandre Rohlandt <jrohlandt@gmail.com>
2019-08-17 12:07:13 +0200

    can now start tree at specified id

--- src/utils/tree.js
+++ src/utils/tree.js
@@ -1,66 +1,68 @@
-function getRoot(flat) {
-    
-  for (let i = 0; i < flat.length; i++) {
-      let item = {...flat[i]};
-      if (item.parent_id === 0) {
-          item.nodes = [];
-          return item;
-      }
+function getRoot(flat, parentId = 0) {
+  let flatShallowClone = [...flat];
+  for (let i = 0; i < flatShallowClone.length; i++) {
+    let item = { ...flatShallowClone[i] };
+    if (item.parent_id === parentId) {
+      item.nodes = [];
+      return item;
+    }
   }
 
   return {};
 }
 
 function getNodes(parentId, flat) {
+  let flatShallowClone = [...flat];
   let nodes = [];
-  for (let i = 0; i < flat.length; i++) {
-      let item = {...flat[i]};
-      if (item.parent_id === parentId) {
-          item.nodes = getNodes(item.id, flat);
-          nodes.push(item);
-      }
+
+  for (let i = 0; i < flatShallowClone.length; i++) {
+    let item = { ...flatShallowClone[i] };
+
+    if (item.parent_id === parentId) {
+      item.nodes = getNodes(item.id, flatShallowClone);
+      nodes.push(item);
+    }
   }
-  nodes.sort((a,b) => a.id -  b.id);
+  nodes.sort((a, b) => a.id - b.id);
   return nodes;
 }
 
-export const buildStructure = function(flat) {
-  
-  let result = getRoot(flat);
+export const buildStructure = function(flat, rootId = 0) {
+  let result = rootId === 0 ? getRoot(flat) : findNodeById(flat, rootId);
   if (result.length === 0) {
-      return result;
+    return result;
   }
 
-  result.nodes = getNodes(result.id, flat)
+  let node = { ...result };
+  node.nodes = getNodes(node.id, flat);
 
-  return result;
+  return node;
 };
 
 export const flattenStructure = function(tree) {
-
   let nodes = [];
   let node = {
-      id: tree.id,
-      parent_id: tree.parent_id,
-      title: tree.title,
-      url: tree.url,
+    id: tree.id,
+    parent_id: tree.parent_id,
+    title: tree.title,
+    url: tree.url
   };
   nodes.push(node);
 
-  if (typeof tree.nodes !== 'undefined' && tree.nodes.length > 0) {
-      for (let i=0; i < tree.nodes.length; i++) {
-          const result = flattenStructure(tree.nodes[i]);
-          for (let i=0; i < result.length; i++) {
-              nodes.push(result[i]);
-          }
+  if (typeof tree.nodes !== "undefined" && tree.nodes.length > 0) {
+    for (let i = 0; i < tree.nodes.length; i++) {
+      const result = flattenStructure(tree.nodes[i]);
+      for (let i = 0; i < result.length; i++) {
+        nodes.push(result[i]);
       }
+    }
   }
 
-  nodes = nodes.sort((a,b) => a.id - b.id);
+  nodes = nodes.sort((a, b) => a.id - b.id);
   return nodes;
-}
+};
 
 export const findNodeById = function(nodes, id) {
-    const filtered = nodes.filter(n => n.id === id);
-    return filtered.length > 0 ? filtered[0] : null;
-}
+  const filtered = nodes.filter(n => n.id === id);
+  return filtered.length > 0 ? filtered[0] : null;
+};
--- src/utils/tree.test.js
+++ src/utils/tree.test.js
@@ -1,136 +1,135 @@
-import { buildStructure, flattenStructure } from './tree.js';
+import { buildStructure, flattenStructure } from "./tree.js";
 
-test('it builds structure', () => {
-    let structure = buildStructure(flat);
-    expect(structure).toEqual(tree);
+test("it builds structure", () => {
+  let structure = buildStructure(flat);
+  expect(structure).toEqual(tree);
 });
 
-test('it flattens structure', () => {
-    let flattened = flattenStructure(tree);
-    expect(flattened).toEqual(flat.sort((a,b) => a.id - b.id));
+test("it builds structure starting with specified parent id", () => {
+  let structure = buildStructure(flat, 1888);
+  expect(structure.id).toBe(1888);
+});
+
+test("it flattens structure", () => {
+  let flattened = flattenStructure(tree);
+  expect(flattened).toEqual(flat.sort((a, b) => a.id - b.id));
 });
 
 const flat = [
-    {
-        id: 1888,
-        parent_id: 10,
-        title: 'child 1',
-        url: 'https://www.youtube.com/watch?v=Sh6lK57Cuk4',
-    },
+  {
+    id: 1888,
+    parent_id: 10,
+    title: "child 1",
+    url: "https://www.youtube.com/watch?v=Sh6lK57Cuk4"
+  },
 
-    {
-        id: 10,
-        parent_id: 0,
-        title: 'parent',
-        url: 'https://www.youtube.com/watch?v=Sh6lK57Cuk4',
-    },
+  {
+    id: 10,
+    parent_id: 0,
+    title: "parent",
+    url: "https://www.youtube.com/watch?v=Sh6lK57Cuk4"
+  },
 
-    {
-        id: 3903209,
-        parent_id: 33,
-        title: 'great grand child 2',
-        url: '',
-    },
+  {
+    id: 3903209,
+    parent_id: 33,
+    title: "great grand child 2",
+    url: ""
+  },
 
-    {
-        id: 677,
-        parent_id: 1888,
-        title: 'grand child 1',
-        url: '',
-    },
+  {
+    id: 677,
+    parent_id: 1888,
+    title: "grand child 1",
+    url: ""
+  },
 
-    {
-        id: 1889,
-        parent_id: 10,
-        title: 'child 2',
-        url: '',
-    }, 
+  {
+    id: 1889,
+    parent_id: 10,
+    title: "child 2",
+    url: ""
+  },
 
-    {
-        id: 33,
-        parent_id: 1889,
-        title: 'grand child 2',
-        url: '',
-    },
+  {
+    id: 33,
+    parent_id: 1889,
+    title: "grand child 2",
+    url: ""
+  },
 
-    {
-        id: 2903209,
-        parent_id: 33,
-        title: 'great grand child 1',
-        url: '',
-    },
+  {
+    id: 2903209,
+    parent_id: 33,
+    title: "great grand child 1",
+    url: ""
+  },
 
-    {
-        id: 34,
-        parent_id: 1889,
-        title: 'grand child 3',
-        url: '',
-    },
-
-
+  {
+    id: 34,
+    parent_id: 1889,
+    title: "grand child 3",
+    url: ""
+  }
 ];
 
 const tree = {
-    id: 10,
-    parent_id: 0,
-    title: 'parent',
-    url: 'https://www.youtube.com/watch?v=Sh6lK57Cuk4',
-    nodes: [
+  id: 10,
+  parent_id: 0,
+  title: "parent",
+  url: "https://www.youtube.com/watch?v=Sh6lK57Cuk4",
+  nodes: [
+    {
+      id: 1888,
+      parent_id: 10,
+      title: "child 1",
+      url: "https://www.youtube.com/watch?v=Sh6lK57Cuk4",
+      nodes: [
         {
-            id: 1888,
-            parent_id: 10,
-            title: 'child 1',
-            url: 'https://www.youtube.com/watch?v=Sh6lK57Cuk4',
-            nodes: [
-                {
-                    id: 677,
-                    parent_id: 1888,
-                    title: 'grand child 1',
-                    url: '',
-                    nodes: [],
-                },
-            ] 
+          id: 677,
+          parent_id: 1888,
+          title: "grand child 1",
+          url: "",
+          nodes: []
+        }
+      ]
+    },
+    {
+      id: 1889,
+      parent_id: 10,
+      title: "child 2",
+      url: "",
+      nodes: [
+        {
+          id: 33,
+          parent_id: 1889,
+          title: "grand child 2",
+          url: "",
+          nodes: [
+            {
+              id: 2903209,
+              parent_id: 33,
+              title: "great grand child 1",
+              url: "",
+              nodes: []
+            },
+            {
+              id: 3903209,
+              parent_id: 33,
+              title: "great grand child 2",
+              url: "",
+              nodes: []
+            }
+          ]
         },
         {
-            id: 1889,
-            parent_id: 10,
-            title: 'child 2',
-            url: '',
-            nodes: [
-                {
-                    id: 33,
-                    parent_id: 1889,
-                    title: 'grand child 2',
-                    url: '',
-                    nodes: [
-                        {
-                            id: 2903209,
-                            parent_id: 33,
-                            title: 'great grand child 1',
-                            url: '',
-                            nodes: [],
-                        },
-                        {
-                            id: 3903209,
-                            parent_id: 33,
-                            title: 'great grand child 2',
-                            url: '',
-                            nodes: [],
-                        }
-                    ] 
-                },
-                {
-                    id: 34,
-                    parent_id: 1889,
-                    title: 'grand child 3',
-                    url: '',
-                    nodes: [],
-                }
-            ] 
+          id: 34,
+          parent_id: 1889,
+          title: "grand child 3",
+          url: "",
+          nodes: []
         }
-    ] 
+      ]
+    }
+  ]
 };
-
-
-
-
